15812290537f852c77d23de447c7dea1a50e7d4b
Renzo Frigato <rfrigato@stanford.edu>
2016-01-29 10:34:03 -0800

    add json schema for reaper

--- api/validators.py
+++ api/validators.py
@@ -43,7 +43,8 @@ expected_input_schemas = set([
     'avatars.json',
     'download.json',
     'tag.json',
-    'enginemetadata.json'
+    'enginemetadata.json',
+    'reaper.json'
 ])
 mongo_schemas = set()
 input_schemas = set()
